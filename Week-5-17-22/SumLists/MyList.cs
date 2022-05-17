328629eb1d3b73b6719d75dd3aae6def07816f2b
Kristian Perez <kpere192@fiu.edu>
2022-05-17 13:30:24 -0400

    SumList working with reading list in reverse order

---
+++ Week-5-17-22/SumLists/MyList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumLists
+{
+    class MyList
+    {
+        public LinkedList<int> sumLists(LinkedList<int> a, LinkedList<int> b)
+        {
+            int aNum = convertToNum(a);
+            int bNum = convertToNum(b);
+            int sum = aNum + bNum;
+            LinkedList<int> sumList = new LinkedList<int>();
+            sumList = convertToList(sum);
+            return sumList;
+        }
+
+        public int convertToNum(LinkedList<int> a)
+        {
+            int output = 0;
+            for (int i = 0; i < a.Count; i++)
+            {
+                output = output + (int)(a.ElementAt(i) * Math.Pow(10, i));
+            }
+            return output;
+        }
+        
+        public LinkedList<int> convertToList(int a)
+        {
+            int digits = 0;
+            int temp = a;
+            LinkedList<int> output = new LinkedList<int>();
+
+            while (temp > 0)
+            {
+                temp = temp / 10;
+                digits++;
+            }
+            
+            for(int i = 0; i < digits; i++)
+            {
+                output.AddLast((int)(a % Math.Pow(10, i+1) / Math.Pow(10,i)));
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Week-5-17-22/SumLists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> a = new LinkedList<int>();
+            LinkedList<int> b = new LinkedList<int>();
+            LinkedList<int> output = new LinkedList<int>();
+            MyList myList = new MyList();
+            a.AddLast(1);
+            a.AddLast(2);
+            a.AddLast(3);
+            b.AddLast(4);
+            b.AddLast(5);
+            b.AddLast(6);
+            output = myList.sumLists(a, b);
+            for(int i = 0; i < output.Count; i++)
+            {
+                if( i != output.Count - 1)
+                    Console.Write("[" + output.ElementAt(i) + "] -> ");
+                else
+                    Console.Write("[" + output.ElementAt(i) + "] = ");
+            }
+            Console.WriteLine(myList.convertToNum(output));
+        }
+    }
+}
