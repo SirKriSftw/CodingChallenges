efa9566d9d68d98644b4d2649b0e152176575bd9
Kristian Perez <kpere192@fiu.edu>
2022-05-17 14:17:54 -0400

    Added random input

--- Week-5-17-22/SumLists/Program.cs
+++ Week-5-17-22/SumLists/Program.cs
@@ -15,14 +15,19 @@ namespace SumLists
             MyList myList = new MyList();
             #endregion
 
-            #region Adding values to both lists and printing
-            a.AddLast(7);
-            a.AddLast(1);
-            a.AddLast(6);
-            b.AddLast(5);
-            b.AddLast(9);
-            b.AddLast(2);
-
+            #region Adding random values to both lists and printing
+            Random rand = new Random();
+            int aSize = rand.Next(1, 6);
+            int bSize = rand.Next(1, 6);
+            for(int i = 0; i < aSize; i++)
+            {
+                a.AddLast(rand.Next(0,9));
+            }
+            for(int i = 0; i < bSize; i++)
+            {
+                b.AddLast(rand.Next(0,9));
+            }
+            
             Console.Write("\t");
             for(int i = 0; i < a.Count; i++)
             {
@@ -56,14 +61,18 @@ namespace SumLists
             #endregion
 
             #region Reverse order of input and printing
+            LinkedList<int> copyA = a;
+            LinkedList<int> copyB = b;
             a = new LinkedList<int>();
             b = new LinkedList<int>();
-            a.AddLast(6);
-            a.AddLast(1);
-            a.AddLast(7);
-            b.AddLast(2);
-            b.AddLast(9);
-            b.AddLast(5);
+            for(int i = 0; i < copyA.Count; i++)
+            {
+                a.AddFirst(copyA.ElementAt(i));
+            }
+            for(int i = 0; i < copyB.Count; i++)
+            {
+                b.AddFirst(copyB.ElementAt(i));
+            }
 
             Console.Write("\n\n");
             Console.Write("\t");
