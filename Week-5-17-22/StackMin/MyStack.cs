1e95ff68a5480d052f72acaeb386de1774240e71
Kristian Perez <kpere192@fiu.edu>
2022-05-17 15:11:44 -0400

    StackMin problem: Get min of stack using O(1). Has user input.

---
+++ Week-5-17-22/StackMin/MyStack.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackMin
+{
+    class MyStack
+    {
+        // Actual stack of numbers
+        Stack<int> myStack = new Stack<int>();
+        // Stack to keep track of the minimum
+        Stack<int> minStack = new Stack<int>();
+
+        /*
+            A method used to add items to myStack
+        */
+        public void push(int a)
+        {
+            // Call normal push stack call
+            myStack.Push(a);
+
+            // IF this is the first item
+            if(myStack.Count == 1)
+            {
+                // Push into min stack
+                minStack.Push(a);
+            }
+
+            else
+            {
+                // IF pushed num is <= current minimum
+                    // = part is important in case of repeating numbers
+                    // IE stack: 1 | 2 | 1, when first 1 pops, second 1 is still min
+                if(a <= minStack.Peek())
+                {
+                    // Push into min stack
+                    minStack.Push(a);
+                }
+
+            }
+        }
+
+        /*
+            A method used to remove top item from stack and return its value
+        */
+        public int pop()
+        {
+            // Call normal stack pop
+            int popped = myStack.Pop();
+
+            // IF poppped number is == the top of the minStack, that means it is current stack
+            if(popped == minStack.Peek())
+            {
+                // So also remove it from minStack to update current min
+                minStack.Pop();
+            }
+            return popped;
+        }
+
+        /*
+            A method used to get the current min for MyStack
+        */
+        public int min()
+        {
+            // Returns top of minStack which contains the current min
+            return minStack.Peek();
+        }
+
+        /*
+            Helper method to see top of MyStack
+        */
+        public int peek()
+        {
+            return myStack.Peek();
+        }
+
+        /*
+            Helper method to print out the current stack by converting it into an array
+        */
+        public void print()
+        {
+            int[] displayArray = myStack.ToArray();
+            Console.Write("Currently in stack: [");
+            for(int i = 0; i < myStack.Count; i++)
+            {
+                if(i != myStack.Count - 1)
+                    Console.Write(" " + displayArray[i] + " |");
+                else
+                    Console.Write(" " + displayArray[i] + " ]\n");
+            }
+        }
+    }
+}
---
+++ Week-5-17-22/StackMin/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace StackMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // MyStack object
+            MyStack myStack = new MyStack();
+            // Random object for getting random numbers to push
+            Random rand = new Random();
+
+            #region Variables for getting user input
+            int stackSize = 0;
+            int rangeMin = 0;
+            int rangeMax = 0;
+            bool canProceed = false;
+            #endregion
+
+            #region Stack Size input w/ validation
+            while(!canProceed)
+            {
+                Console.Write("Stack Size: ");
+                string inputSize = Console.ReadLine();
+                bool isNum = int.TryParse(inputSize, out stackSize);
+                if(isNum)
+                {
+                    if(stackSize <= 0)
+                    {
+                        Console.Write("\nEnter a valid size (greater than 0)\n");
+                    }
+                    else
+                    {
+                        canProceed = true;
+                    }
+                }
+                else
+                {
+                    Console.Write("\nStack Size must be a number\n");
+                }
+            }
+            #endregion
+
+            #region Stack Range input w/ validation
+            canProceed = false;
+            while(!canProceed)
+            {
+                Console.Write("Range Min: ");
+                string inputRange = Console.ReadLine();
+                bool isNum = int.TryParse(inputRange, out rangeMin);
+                if(isNum)
+                {
+                    Console.Write("Range Max: ");
+                    inputRange = Console.ReadLine();
+                    isNum = int.TryParse(inputRange, out rangeMax);
+                    if (isNum)
+                    {
+                        if(rangeMin > rangeMax)
+                        {
+                            Console.Write("\nRange min cannot be greater than range max\n");
+                        }
+                        else
+                        {
+                            canProceed = true;
+                        }                        
+                    }
+                    else
+                    {
+                        Console.Write("\nRange max must be a number\n");
+                    }
+                }
+                else
+                {
+                    Console.Write("\nRange min must be a number\n");
+                }
+            }
+            #endregion
+
+            #region Push into stack and print pushed, stack and min
+            for(int i = 0; i < stackSize; i++)
+            {
+                myStack.push(rand.Next(rangeMin,rangeMax+1));
+                Console.WriteLine("\nPushed: " + myStack.peek());
+                myStack.print();
+                Console.WriteLine("Current min: " + myStack.min());                                
+            }
+            #endregion
+
+            #region Pop from stack and print popped, stack, and min
+            for(int i = 0; i < stackSize; i++)
+            {
+                Console.WriteLine("\nPopped: " + myStack.pop());
+                
+                if (i != stackSize - 1)
+                {
+                    myStack.print();
+                    Console.WriteLine("Current min: " + myStack.min());
+                }
+                else
+                {
+                    Console.WriteLine("Stack emptied");
+                }
+            }
+            #endregion
+        }
+    }
+}
